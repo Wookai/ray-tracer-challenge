c7380bf7abda748eda3a6012cc7e619bb88f7f8e
Vincent Etter <vincent@etter.io>
2022-10-04 06:40:17 +0200

    Add Color class with tests.

---
+++ RayTracerChallenge/RayTracerChallenge/Color.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RayTracerChallenge
+{
+    public readonly struct Color
+    {
+        public readonly float red;
+        public readonly float green;
+        public readonly float blue;
+
+        public Color(float r, float g, float b) { red = r; green = g; blue = b; }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Color c = (Color)obj;
+            return c.red == red && c.green == green && c.blue == blue;
+        }
+
+        public static bool operator ==(Color left, Color right) => left.Equals(right);
+        public static bool operator !=(Color left, Color right) => !(left == right);
+
+        public override int GetHashCode() => Tuple.Create(red, green, blue).GetHashCode();
+
+        public static Color operator +(Color a, Color b) => new(a.red + b.red, a.green + b.green, a.blue + b.blue);
+        public static Color operator -(Color a, Color b) => new(a.red - b.red, a.green - b.green, a.blue - b.blue);
+        public static Color operator *(Color a, float b) => new(a.red * b, a.green * b, a.blue * b);
+        public static Color operator *(float a, Color b) => b * a;
+        public static Color operator *(Color a, Color b) => new(a.red * b.red, a.green * b.green, a.blue * b.blue);
+
+    }
+}
---
+++ RayTracerChallenge/Tests/ColorTest.cs
@@ -0,0 +1,62 @@
+﻿using RayTracerChallenge;
+
+namespace Tests
+{
+    [TestClass]
+    public class ColorTest
+    {
+        [TestMethod]
+        public void TestCreateColor()
+        {
+            var c = new Color(-0.5f, 0.4f, 1.7f);
+            Assert.AreEqual(-0.5, c.red, 1e-3);
+            Assert.AreEqual(0.4, c.green, 1e-3);
+            Assert.AreEqual(1.7, c.blue, 1e-3);
+        }
+
+        [TestMethod]
+        public void TestEquality()
+        {
+            var a = new Color(-0.5f, 0.4f, 1.7f);
+            var b = new Color(-0.5f, 0.4f, 1.7f);
+            var c = new Color(1, 2, 3);
+            Assert.IsTrue(a.Equals(b));
+            Assert.IsFalse(a.Equals(c));
+            Assert.IsTrue(a == b);
+            Assert.IsTrue(a != c);
+        }
+
+        [TestMethod]
+        public void TestAddition()
+        {
+            var sum = new Color(0.9f, 0.6f, 0.75f) + new Color(0.7f, 0.1f, 0.25f);
+            Assert.AreEqual(1.6, sum.red, 1e-3);
+            Assert.AreEqual(0.7, sum.green, 1e-3);
+            Assert.AreEqual(1.0, sum.blue, 1e-3);
+        }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            var difference = new Color(0.9f, 0.6f, 0.75f) - new Color(0.7f, 0.1f, 0.15f);
+            Assert.AreEqual(0.2, difference.red, 1e-3);
+            Assert.AreEqual(0.5, difference.green, 1e-3);
+            Assert.AreEqual(0.6, difference.blue, 1e-3);
+        }
+
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            var c = new Color(0.2f, 0.3f, 0.4f);
+            var twoC = 2 * c;
+            Assert.AreEqual(0.4, twoC.red, 1e-3);
+            Assert.AreEqual(0.6, twoC.green, 1e-3);
+            Assert.AreEqual(0.8, twoC.blue, 1e-3);
+
+            var product = new Color(1, 0.2f, 0.4f) * new Color(0.9f, 1, 0.1f);
+            Assert.AreEqual(0.9f, product.red, 1e-3);
+            Assert.AreEqual(0.2f, product.green, 1e-3);
+            Assert.AreEqual(0.04f, product.blue, 1e-3);
+        }
+    }
+}

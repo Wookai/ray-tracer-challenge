64eeb71be44e848669386f8fc2af7b9cc7e32f7f
Vincent Etter <vincent@etter.io>
2022-10-04 07:01:52 +0200

    Add Canvas class with beginning of PPM output.

---
+++ RayTracerChallenge/RayTracerChallenge/Canvas.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace RayTracerChallenge
+{
+    public class Canvas
+    {
+        private readonly Color[,] pixels;
+        public int Height;
+        public int Width;
+
+        public Canvas(int width, int height)
+        {
+            Height = height;
+            Width = width;
+            pixels = new Color[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    pixels[x, y] = new Color(0, 0, 0);
+                }
+            }
+        }
+
+        public void WritePixel(int x, int y, Color c) => pixels[x, y] = c;
+        public Color GetPixel(int x, int y) => pixels[x, y];
+
+        public string ToPPM()
+        {
+            StringBuilder sb = new();
+            sb.Append("P3\n");
+            sb.AppendFormat("{0} {1}\n", Width, Height);
+            sb.Append("255\n");
+            return sb.ToString();
+        }
+    }
+}
+
---
+++ RayTracerChallenge/Tests/CanvasTest.cs
@@ -0,0 +1,39 @@
+﻿using RayTracerChallenge;
+
+namespace Tests
+{
+    [TestClass]
+    public class CanvasTest
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            var canvas = new Canvas(10, 20);
+            Assert.AreEqual(10, canvas.Width);
+            Assert.AreEqual(20, canvas.Height);
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    Assert.AreEqual(new Color(0, 0, 0), canvas.GetPixel(x, y));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestWritePixel()
+        {
+            var canvas = new Canvas(10, 20);
+            var red = new Color(1, 0, 0);
+            canvas.WritePixel(2, 3, red);
+            Assert.AreEqual(red, canvas.GetPixel(2, 3));
+        }
+
+        [TestMethod]
+        public void TestToPPM()
+        {
+            var canvas = new Canvas(5, 3);
+            Assert.AreEqual("P3\n5 3\n255\n", canvas.ToPPM());
+        }
+    }
+}
